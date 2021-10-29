30df818d86071211f0e400d68558e7cabcb8e5c9
FieryInferno <bagassetia271@gmail.com>
2021-10-29 13:02:58 +0700

    Make profile edit view

--- app/src/main/java/com/example/pos/EditProfile.java
+++ app/src/main/java/com/example/pos/EditProfile.java
@@ -2,7 +2,9 @@ package com.example.pos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class EditProfile extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class EditProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+    }
+
+    public void profile(View view) {
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
     }
 }
