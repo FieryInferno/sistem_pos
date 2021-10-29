fe459e5627fa7fbeeea00cd7f389c47a02b5f3d1
FieryInferno <bagassetia271@gmail.com>
2021-10-29 12:48:22 +0700

    Make profile view

--- app/src/main/java/com/example/pos/Profile.java
+++ app/src/main/java/com/example/pos/Profile.java
@@ -6,21 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class DashboardUser extends AppCompatActivity {
+public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard_user);
+        setContentView(R.layout.activity_profile);
     }
 
-    public void scanQR(View view) {
-        Intent intent = new Intent(this, ScanQR.class);
+    public void dashboardUser(View view) {
+        Intent intent = new Intent(this, DashboardUser.class);
         startActivity(intent);
     }
 
-    public void profile(View view) {
-        Intent intent = new Intent(this, Profile.class);
+    public void editProfile(View view) {
+        Intent intent = new Intent(this, EditProfile.class);
         startActivity(intent);
     }
 }
