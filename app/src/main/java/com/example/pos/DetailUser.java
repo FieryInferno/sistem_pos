8866a36899e3be4ab875dc536843f95ed8f6a4f4
FieryInferno <bagassetia271@gmail.com>
2021-11-02 09:23:44 +0700

    Make detail user view

--- app/src/main/java/com/example/pos/DetailUser.java
+++ app/src/main/java/com/example/pos/DetailUser.java
@@ -2,7 +2,9 @@ package com.example.pos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DetailUser extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class DetailUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_user);
+    }
+
+    public void dashboardAdmin(View view) {
+        Intent intent = new Intent(this, DashboardAdmin.class);
+        startActivity(intent);
     }
 }
